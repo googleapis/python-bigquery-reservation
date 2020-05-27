c9f29d527305cd9921d441a8b65ad1770648f358
yoshi-automation <yoshi-automation@google.com>
2020-05-27 10:12:33 -0700

    docs: Properly format literal strings.
    PiperOrigin-RevId: 313202929
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue May 26 09:12:28 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: f563b1dffee42be2f61c326c39af4974c1984278
    Source-Link: https://github.com/googleapis/googleapis/commit/f563b1dffee42be2f61c326c39af4974c1984278

--- google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
@@ -163,24 +163,6 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
         return m.groupdict() if m else {}
 
     @staticmethod
-    def capacity_commitment_path(
-        project: str, location: str, capacity_commitment: str
-    ) -> str:
-        """Return a fully-qualified capacity_commitment string."""
-        return "projects/{project}/locations/{location}/capacityCommitments/{capacity_commitment}".format(
-            project=project, location=location, capacity_commitment=capacity_commitment
-        )
-
-    @staticmethod
-    def parse_capacity_commitment_path(path: str) -> Dict[str, str]:
-        """Parse a capacity_commitment path into its component segments."""
-        m = re.match(
-            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/capacityCommitments/(?P<capacity_commitment>.+?)$",
-            path,
-        )
-        return m.groupdict() if m else {}
-
-    @staticmethod
     def assignment_path(
         project: str, location: str, reservation: str, assignment: str
     ) -> str:
@@ -197,6 +179,24 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
         """Parse a assignment path into its component segments."""
         m = re.match(
             r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/reservations/(?P<reservation>.+?)/assignments/(?P<assignment>.+?)$",
+            path,
+        )
+        return m.groupdict() if m else {}
+
+    @staticmethod
+    def capacity_commitment_path(
+        project: str, location: str, capacity_commitment: str
+    ) -> str:
+        """Return a fully-qualified capacity_commitment string."""
+        return "projects/{project}/locations/{location}/capacityCommitments/{capacity_commitment}".format(
+            project=project, location=location, capacity_commitment=capacity_commitment
+        )
+
+    @staticmethod
+    def parse_capacity_commitment_path(path: str) -> Dict[str, str]:
+        """Parse a capacity_commitment path into its component segments."""
+        m = re.match(
+            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/capacityCommitments/(?P<capacity_commitment>.+?)$",
             path,
         )
         return m.groupdict() if m else {}
