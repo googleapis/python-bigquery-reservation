5ace2f3468391499721dc18cdc16e4ef6b4b0d00
yoshi-automation <yoshi-automation@google.com>
2020-05-27 10:06:08 -0700

    chore: move ci.yaml to workflows dir (#535)
    Source-Author: Stephanie Wang <stephaniewang526@users.noreply.github.com>
    Source-Date: Mon May 11 16:24:04 2020 -0400
    Source-Repo: googleapis/synthtool
    Source-Sha: 98c50772ec23295c64cf0d2ddf199ea52961fd4c
    Source-Link: https://github.com/googleapis/synthtool/commit/98c50772ec23295c64cf0d2ddf199ea52961fd4c

--- google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
@@ -165,6 +165,22 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
         return m.groupdict() if m else {}
 
     @staticmethod
+    def reservation_path(project: str, location: str, reservation: str) -> str:
+        """Return a fully-qualified reservation string."""
+        return "projects/{project}/locations/{location}/reservations/{reservation}".format(
+            project=project, location=location, reservation=reservation
+        )
+
+    @staticmethod
+    def parse_reservation_path(path: str) -> Dict[str, str]:
+        """Parse a reservation path into its component segments."""
+        m = re.match(
+            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/reservations/(?P<reservation>.+?)$",
+            path,
+        )
+        return m.groupdict() if m else {}
+
+    @staticmethod
     def assignment_path(
         project: str, location: str, reservation: str, assignment: str
     ) -> str:
@@ -197,22 +213,6 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
         """Parse a bi_reservation path into its component segments."""
         m = re.match(
             r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/bireservation$",
-            path,
-        )
-        return m.groupdict() if m else {}
-
-    @staticmethod
-    def reservation_path(project: str, location: str, reservation: str) -> str:
-        """Return a fully-qualified reservation string."""
-        return "projects/{project}/locations/{location}/reservations/{reservation}".format(
-            project=project, location=location, reservation=reservation
-        )
-
-    @staticmethod
-    def parse_reservation_path(path: str) -> Dict[str, str]:
-        """Parse a reservation path into its component segments."""
-        m = re.match(
-            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/reservations/(?P<reservation>.+?)$",
             path,
         )
         return m.groupdict() if m else {}
