5ba174b4fc8cd51992b31cb361d1a58c3e58acb2
yoshi-automation <yoshi-automation@google.com>
2020-05-29 06:22:58 -0700

    feat: add templates for python samples projects (#506)
    These templates will be used for templates in python-docs-samples
    and in Python client libraries. The README generation code is a modified
    version of
    https://github.com/GoogleCloudPlatform/python-docs-samples/tree/master/scripts/readme-gen.
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    Source-Author: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    Source-Date: Thu May 28 14:39:58 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: ffe10407ee2f261c799fb0d01bf32a8abc67ed1e
    Source-Link: https://github.com/googleapis/synthtool/commit/ffe10407ee2f261c799fb0d01bf32a8abc67ed1e

--- tests/unit/reservation_v1/test_reservation_service.py
+++ tests/unit/reservation_v1/test_reservation_service.py
@@ -2115,6 +2115,33 @@ def test_parse_assignment_path():
     assert expected == actual
 
 
+def test_capacity_commitment_path():
+    project = "squid"
+    location = "clam"
+    capacity_commitment = "whelk"
+
+    expected = "projects/{project}/locations/{location}/capacityCommitments/{capacity_commitment}".format(
+        project=project, location=location, capacity_commitment=capacity_commitment
+    )
+    actual = ReservationServiceClient.capacity_commitment_path(
+        project, location, capacity_commitment
+    )
+    assert expected == actual
+
+
+def test_parse_capacity_commitment_path():
+    expected = {
+        "project": "octopus",
+        "location": "oyster",
+        "capacity_commitment": "nudibranch",
+    }
+    path = ReservationServiceClient.capacity_commitment_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = ReservationServiceClient.parse_capacity_commitment_path(path)
+    assert expected == actual
+
+
 def test_reservation_path():
     project = "squid"
     location = "clam"
@@ -2153,31 +2180,4 @@ def test_parse_bi_reservation_path():
 
     # Check that the path construction is reversible.
     actual = ReservationServiceClient.parse_bi_reservation_path(path)
-    assert expected == actual
-
-
-def test_capacity_commitment_path():
-    project = "squid"
-    location = "clam"
-    capacity_commitment = "whelk"
-
-    expected = "projects/{project}/locations/{location}/capacityCommitments/{capacity_commitment}".format(
-        project=project, location=location, capacity_commitment=capacity_commitment
-    )
-    actual = ReservationServiceClient.capacity_commitment_path(
-        project, location, capacity_commitment
-    )
-    assert expected == actual
-
-
-def test_parse_capacity_commitment_path():
-    expected = {
-        "project": "octopus",
-        "location": "oyster",
-        "capacity_commitment": "nudibranch",
-    }
-    path = ReservationServiceClient.capacity_commitment_path(**expected)
-
-    # Check that the path construction is reversible.
-    actual = ReservationServiceClient.parse_capacity_commitment_path(path)
     assert expected == actual
