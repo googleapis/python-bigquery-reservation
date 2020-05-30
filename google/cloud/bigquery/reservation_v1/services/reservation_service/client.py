423a9c167ff9ab4fc5b51aacfb085d57f37b5ec2
yoshi-automation <yoshi-automation@google.com>
2020-05-30 05:21:16 -0700

    docs: update python docs template (#576)
    * docs: update python docs template
    
    * sphinx change
    
    Source-Author: kolea2 <45548808+kolea2@users.noreply.github.com>
    Source-Date: Wed May 27 20:44:34 2020 -0400
    Source-Repo: googleapis/synthtool
    Source-Sha: 71b8a272549c06b5768d00fa48d3ae990e871bec
    Source-Link: https://github.com/googleapis/synthtool/commit/71b8a272549c06b5768d00fa48d3ae990e871bec

--- google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/client.py
@@ -147,6 +147,27 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
     from_service_account_json = from_service_account_file
 
     @staticmethod
+    def assignment_path(
+        project: str, location: str, reservation: str, assignment: str
+    ) -> str:
+        """Return a fully-qualified assignment string."""
+        return "projects/{project}/locations/{location}/reservations/{reservation}/assignments/{assignment}".format(
+            project=project,
+            location=location,
+            reservation=reservation,
+            assignment=assignment,
+        )
+
+    @staticmethod
+    def parse_assignment_path(path: str) -> Dict[str, str]:
+        """Parse a assignment path into its component segments."""
+        m = re.match(
+            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/reservations/(?P<reservation>.+?)/assignments/(?P<assignment>.+?)$",
+            path,
+        )
+        return m.groupdict() if m else {}
+
+    @staticmethod
     def bi_reservation_path(project: str, location: str) -> str:
         """Return a fully-qualified bi_reservation string."""
         return "projects/{project}/locations/{location}/bireservation".format(
@@ -192,27 +213,6 @@ class ReservationServiceClient(metaclass=ReservationServiceClientMeta):
         """Parse a capacity_commitment path into its component segments."""
         m = re.match(
             r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/capacityCommitments/(?P<capacity_commitment>.+?)$",
-            path,
-        )
-        return m.groupdict() if m else {}
-
-    @staticmethod
-    def assignment_path(
-        project: str, location: str, reservation: str, assignment: str
-    ) -> str:
-        """Return a fully-qualified assignment string."""
-        return "projects/{project}/locations/{location}/reservations/{reservation}/assignments/{assignment}".format(
-            project=project,
-            location=location,
-            reservation=reservation,
-            assignment=assignment,
-        )
-
-    @staticmethod
-    def parse_assignment_path(path: str) -> Dict[str, str]:
-        """Parse a assignment path into its component segments."""
-        m = re.match(
-            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/reservations/(?P<reservation>.+?)/assignments/(?P<assignment>.+?)$",
             path,
         )
         return m.groupdict() if m else {}
