353fbd2d244cd70529866144ab5bcaf6e2ce3395
yoshi-automation <yoshi-automation@google.com>
2020-07-31 07:59:37 -0700

    chore: upgrade to gapic-generator-python 0.29.1
    PiperOrigin-RevId: 322700241
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jul 22 18:41:34 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: a1c9dba23852947c0232f7c13dd1e92108f92153
    Source-Link: https://github.com/googleapis/googleapis/commit/a1c9dba23852947c0232f7c13dd1e92108f92153

--- tests/unit/gapic/reservation_v1/test_reservation_service.py
+++ tests/unit/gapic/reservation_v1/test_reservation_service.py
@@ -5498,24 +5498,23 @@ def test_reservation_service_grpc_asyncio_transport_channel_mtls_with_adc(
         assert transport.grpc_channel == mock_grpc_channel
 
 
-def test_reservation_path():
+def test_bi_reservation_path():
     project = "squid"
     location = "clam"
-    reservation = "whelk"
 
-    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
-        project=project, location=location, reservation=reservation
+    expected = "projects/{project}/locations/{location}/bireservation".format(
+        project=project, location=location
     )
-    actual = ReservationServiceClient.reservation_path(project, location, reservation)
+    actual = ReservationServiceClient.bi_reservation_path(project, location)
     assert expected == actual
 
 
-def test_parse_reservation_path():
-    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
-    path = ReservationServiceClient.reservation_path(**expected)
+def test_parse_bi_reservation_path():
+    expected = {"project": "whelk", "location": "octopus"}
+    path = ReservationServiceClient.bi_reservation_path(**expected)
 
     # Check that the path construction is reversible.
-    actual = ReservationServiceClient.parse_reservation_path(path)
+    actual = ReservationServiceClient.parse_bi_reservation_path(path)
     assert expected == actual
 
 
@@ -5543,6 +5542,27 @@ def test_parse_capacity_commitment_path():
 
     # Check that the path construction is reversible.
     actual = ReservationServiceClient.parse_capacity_commitment_path(path)
+    assert expected == actual
+
+
+def test_reservation_path():
+    project = "squid"
+    location = "clam"
+    reservation = "whelk"
+
+    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
+        project=project, location=location, reservation=reservation
+    )
+    actual = ReservationServiceClient.reservation_path(project, location, reservation)
+    assert expected == actual
+
+
+def test_parse_reservation_path():
+    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
+    path = ReservationServiceClient.reservation_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = ReservationServiceClient.parse_reservation_path(path)
     assert expected == actual
 
 
@@ -5575,24 +5595,4 @@ def test_parse_assignment_path():
 
     # Check that the path construction is reversible.
     actual = ReservationServiceClient.parse_assignment_path(path)
-    assert expected == actual
-
-
-def test_bi_reservation_path():
-    project = "squid"
-    location = "clam"
-
-    expected = "projects/{project}/locations/{location}/bireservation".format(
-        project=project, location=location
-    )
-    actual = ReservationServiceClient.bi_reservation_path(project, location)
-    assert expected == actual
-
-
-def test_parse_bi_reservation_path():
-    expected = {"project": "whelk", "location": "octopus"}
-    path = ReservationServiceClient.bi_reservation_path(**expected)
-
-    # Check that the path construction is reversible.
-    actual = ReservationServiceClient.parse_bi_reservation_path(path)
     assert expected == actual
