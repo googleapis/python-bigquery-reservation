230b8d4f185e8111a1e2c413377e59b30128da24
yoshi-automation <yoshi-automation@google.com>
2020-07-31 08:30:26 -0700

    feat(python-library): changes to docs job
    * feat(python-library): changes to docs job
    
    * migrate to Trampoline V2
    * add docs-presubmit job
    * create docfx yaml files and upload them to another bucket
    
    * remove redundant envvars
    
    * add a failing test first
    
    * fix TemplateSyntaxError: Missing end of comment tag
    
    * serving_path is not needed any more
    
    * use `raw` to make jinja happy
    
    Source-Author: Takashi Matsuo <tmatsuo@google.com>
    Source-Date: Thu Jul 30 12:44:02 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: 5dfda5621df45b71b6e88544ebbb53b1a8c90214
    Source-Link: https://github.com/googleapis/synthtool/commit/5dfda5621df45b71b6e88544ebbb53b1a8c90214

--- google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
@@ -65,13 +65,13 @@ class ReservationServiceAsyncClient:
     DEFAULT_ENDPOINT = ReservationServiceClient.DEFAULT_ENDPOINT
     DEFAULT_MTLS_ENDPOINT = ReservationServiceClient.DEFAULT_MTLS_ENDPOINT
 
-    assignment_path = staticmethod(ReservationServiceClient.assignment_path)
-
     capacity_commitment_path = staticmethod(
         ReservationServiceClient.capacity_commitment_path
     )
 
     reservation_path = staticmethod(ReservationServiceClient.reservation_path)
+
+    assignment_path = staticmethod(ReservationServiceClient.assignment_path)
 
     bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
 
