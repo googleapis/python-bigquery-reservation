53a18ab1ca3b6359c70a3592165814a6d6a5c7fe
yoshi-automation <yoshi-automation@google.com>
2020-07-31 07:04:29 -0700

    chore: update gapic-generator and go microgen
    changes include:
    - build_gen: go lro gapic used as dep
    - go_gapic_library: fixes shading of go_library importpath
    
    PiperOrigin-RevId: 314363155
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Jun 2 10:56:09 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 3a4894c4f0da3e763aca2c67bd280ae915177450
    Source-Link: https://github.com/googleapis/googleapis/commit/3a4894c4f0da3e763aca2c67bd280ae915177450

--- tests/unit/gapic/reservation_v1/test_reservation_service.py
+++ tests/unit/gapic/reservation_v1/test_reservation_service.py
@@ -5545,6 +5545,27 @@ def test_parse_capacity_commitment_path():
     assert expected == actual
 
 
+def test_reservation_path():
+    project = "squid"
+    location = "clam"
+    reservation = "whelk"
+
+    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
+        project=project, location=location, reservation=reservation
+    )
+    actual = ReservationServiceClient.reservation_path(project, location, reservation)
+    assert expected == actual
+
+
+def test_parse_reservation_path():
+    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
+    path = ReservationServiceClient.reservation_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = ReservationServiceClient.parse_reservation_path(path)
+    assert expected == actual
+
+
 def test_assignment_path():
     project = "squid"
     location = "clam"
@@ -5574,25 +5595,4 @@ def test_parse_assignment_path():
 
     # Check that the path construction is reversible.
     actual = ReservationServiceClient.parse_assignment_path(path)
-    assert expected == actual
-
-
-def test_reservation_path():
-    project = "squid"
-    location = "clam"
-    reservation = "whelk"
-
-    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
-        project=project, location=location, reservation=reservation
-    )
-    actual = ReservationServiceClient.reservation_path(project, location, reservation)
-    assert expected == actual
-
-
-def test_parse_reservation_path():
-    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
-    path = ReservationServiceClient.reservation_path(**expected)
-
-    # Check that the path construction is reversible.
-    actual = ReservationServiceClient.parse_reservation_path(path)
     assert expected == actual
