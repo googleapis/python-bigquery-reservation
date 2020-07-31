aff207e2e0ffc531db96fb6bb1970b7c4717d285
yoshi-automation <yoshi-automation@google.com>
2020-07-31 08:14:06 -0700

    fix: use a [CHANG ME] PR title when there's a commit without context
    Source-Author: Jeffrey Rennie <rennie@google.com>
    Source-Date: Thu Jun 11 08:24:17 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: 1605270fc97d691bec96c621d288b37fb2e3aaf7
    Source-Link: https://github.com/googleapis/synthtool/commit/1605270fc97d691bec96c621d288b37fb2e3aaf7

--- tests/unit/gapic/reservation_v1/test_reservation_service.py
+++ tests/unit/gapic/reservation_v1/test_reservation_service.py
@@ -5498,6 +5498,27 @@ def test_reservation_service_grpc_asyncio_transport_channel_mtls_with_adc(
         assert transport.grpc_channel == mock_grpc_channel
 
 
+def test_reservation_path():
+    project = "squid"
+    location = "clam"
+    reservation = "whelk"
+
+    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
+        project=project, location=location, reservation=reservation
+    )
+    actual = ReservationServiceClient.reservation_path(project, location, reservation)
+    assert expected == actual
+
+
+def test_parse_reservation_path():
+    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
+    path = ReservationServiceClient.reservation_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = ReservationServiceClient.parse_reservation_path(path)
+    assert expected == actual
+
+
 def test_bi_reservation_path():
     project = "squid"
     location = "clam"
@@ -5574,25 +5595,4 @@ def test_parse_capacity_commitment_path():
 
     # Check that the path construction is reversible.
     actual = ReservationServiceClient.parse_capacity_commitment_path(path)
-    assert expected == actual
-
-
-def test_reservation_path():
-    project = "squid"
-    location = "clam"
-    reservation = "whelk"
-
-    expected = "projects/{project}/locations/{location}/reservations/{reservation}".format(
-        project=project, location=location, reservation=reservation
-    )
-    actual = ReservationServiceClient.reservation_path(project, location, reservation)
-    assert expected == actual
-
-
-def test_parse_reservation_path():
-    expected = {"project": "octopus", "location": "oyster", "reservation": "nudibranch"}
-    path = ReservationServiceClient.reservation_path(**expected)
-
-    # Check that the path construction is reversible.
-    actual = ReservationServiceClient.parse_reservation_path(path)
     assert expected == actual
