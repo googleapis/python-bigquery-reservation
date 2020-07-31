59459bd5ed341878226a09ecd4a733a15ea66794
yoshi-automation <yoshi-automation@google.com>
2020-07-31 07:46:32 -0700

    redis: v1 add UpgradeInstance request
    PiperOrigin-RevId: 319332818
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jul 1 18:24:23 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 76905ffe7e3b0605f64ef889fb88913634f9f836
    Source-Link: https://github.com/googleapis/googleapis/commit/76905ffe7e3b0605f64ef889fb88913634f9f836

--- google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
@@ -71,9 +71,9 @@ class ReservationServiceAsyncClient:
         ReservationServiceClient.capacity_commitment_path
     )
 
-    bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
-
     assignment_path = staticmethod(ReservationServiceClient.assignment_path)
+
+    bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
 
     from_service_account_file = ReservationServiceClient.from_service_account_file
     from_service_account_json = from_service_account_file
