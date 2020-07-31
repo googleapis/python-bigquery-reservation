8ca78b114b94cac3d2c0377e055ca327f2498542
yoshi-automation <yoshi-automation@google.com>
2020-07-31 08:15:48 -0700

    ci(java): fix typo in java integration test secret name
    Source-Author: BenWhitehead <BenWhitehead@users.noreply.github.com>
    Source-Date: Tue Jun 16 13:32:19 2020 -0400
    Source-Repo: googleapis/synthtool
    Source-Sha: c4f3059c27591eb24d6942a0e357ec94c80459f2
    Source-Link: https://github.com/googleapis/synthtool/commit/c4f3059c27591eb24d6942a0e357ec94c80459f2

--- google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
@@ -65,11 +65,11 @@ class ReservationServiceAsyncClient:
     DEFAULT_ENDPOINT = ReservationServiceClient.DEFAULT_ENDPOINT
     DEFAULT_MTLS_ENDPOINT = ReservationServiceClient.DEFAULT_MTLS_ENDPOINT
 
-    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
-
     bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
 
     assignment_path = staticmethod(ReservationServiceClient.assignment_path)
+
+    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
 
     capacity_commitment_path = staticmethod(
         ReservationServiceClient.capacity_commitment_path
