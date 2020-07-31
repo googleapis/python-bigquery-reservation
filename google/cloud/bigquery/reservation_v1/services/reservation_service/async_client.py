0d9e6bddc2df5f6f28c34296259710b750f5be58
yoshi-automation <yoshi-automation@google.com>
2020-07-31 07:55:28 -0700

    Adds bazel build file for public version of AuditData proto for bigquery.
    PiperOrigin-RevId: 321696862
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jul 16 19:32:25 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb46f87724ddd2fd1ce9f9dcb71f147427775592
    Source-Link: https://github.com/googleapis/googleapis/commit/bb46f87724ddd2fd1ce9f9dcb71f147427775592

--- google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
@@ -69,11 +69,11 @@ class ReservationServiceAsyncClient:
 
     bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
 
+    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
+
     capacity_commitment_path = staticmethod(
         ReservationServiceClient.capacity_commitment_path
     )
-
-    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
 
     from_service_account_file = ReservationServiceClient.from_service_account_file
     from_service_account_json = from_service_account_file
