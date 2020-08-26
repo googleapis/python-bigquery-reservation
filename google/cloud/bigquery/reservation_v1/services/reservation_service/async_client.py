87005b8e327825f0f1477658beb39a85d548a775
yoshi-automation <yoshi-automation@google.com>
2020-08-26 05:46:54 -0700

    Update protobuf workspace dependency to v3.13.0.
    PiperOrigin-RevId: 327026955
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Aug 17 08:40:22 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0dc0a6c0f1a9f979bc0690f0caa5fbafa3000c2c
    Source-Link: https://github.com/googleapis/googleapis/commit/0dc0a6c0f1a9f979bc0690f0caa5fbafa3000c2c

--- google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
+++ google/cloud/bigquery/reservation_v1/services/reservation_service/async_client.py
@@ -65,8 +65,6 @@ class ReservationServiceAsyncClient:
     DEFAULT_ENDPOINT = ReservationServiceClient.DEFAULT_ENDPOINT
     DEFAULT_MTLS_ENDPOINT = ReservationServiceClient.DEFAULT_MTLS_ENDPOINT
 
-    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
-
     bi_reservation_path = staticmethod(ReservationServiceClient.bi_reservation_path)
 
     capacity_commitment_path = staticmethod(
@@ -74,6 +72,8 @@ class ReservationServiceAsyncClient:
     )
 
     assignment_path = staticmethod(ReservationServiceClient.assignment_path)
+
+    reservation_path = staticmethod(ReservationServiceClient.reservation_path)
 
     from_service_account_file = ReservationServiceClient.from_service_account_file
     from_service_account_json = from_service_account_file
